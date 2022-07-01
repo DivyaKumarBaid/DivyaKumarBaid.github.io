500eb2d16838eb3befd97a0cae9fcac957f25f0c
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-07-01 15:25:05 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,8 @@ export default function About() {
                 <div className="hrLine"></div>
             </div>
               <div className="aboutPara">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel nesciunt pariatur nihil expedita hic a ducimus. Deleniti dolorum nulla officia fugit earum iure doloribus dolorem cupiditate animi nihil! Neque, porro.
-                  Minima eos accusamus deserunt quidem accusantium ducimus mollitia! Possimus nobis tempore sed alias dolores odio laudantium! Tempore animi labore inventore repellendus est? Enim aliquam adipisci odit quae accusamus consequatur vel!
-                  Molestias repellat consequuntur non ipsam unde iusto distinctio quod dicta asperiores deleniti reiciendis facere assumenda, corporis perspiciatis debitis quo totam, consectetur facilis veritatis esse omnis, atque sit! Ipsum, inventore cumque!
+          Hi, my name is Divya Kumar Baid and I find WEB a cause for living as it is live XD.
+          Jokes aside I am a currently pursuing Computer Science and Engineering from Techno Main Salt Lake, India. I crave for problems -> related to Tech and enjoy solving them.
               </div>
           </div>
           <div className='aboutImg'>
